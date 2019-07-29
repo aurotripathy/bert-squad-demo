058a35ca7d987b4a592865f200a4993d1f1d485a
Auro Tripathy <ipserv@yahoo.com>
2019-07-29 05:50:01 +0000

    wip

--- upload.py
+++ upload.py
@@ -2,19 +2,20 @@ from flask import Flask, render_template, request
 from werkzeug import secure_filename
 app = Flask(__name__)
 
+# TODO should these be globals
+view_data = {'subject' : '', 'question' : '', 'answer' : ''}
+
 @app.route('/upload')
 def upload_file():
-   # return render_template('upload.html')
-   return render_template('my-form.html')
+   return render_template('my-form.html', result=view_data['subject'])
 
 @app.route('/uploader', methods = ['GET', 'POST'])
 def uploader_file():
    if request.method == 'POST':
-      # f = request.files['file']
-      print(request.form['message'])
-      
-      # f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
+      print(request.form['subject'])
+      view_data['subject'] = request.form['subject']
+      return render_template('my-form.html', result=view_data['subject'])
+
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=5001, debug = True)
