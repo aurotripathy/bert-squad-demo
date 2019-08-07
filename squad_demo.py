9e5d9186f89d842ddc67875627dda57cec7c4b09
Auro Tripathy <ipserv@yahoo.com>
2019-08-07 21:16:00 +0000

    initial

--- squad_demo.py
+++ squad_demo.py
@@ -1,20 +1,33 @@
+
+import sys
+sys.path.append('../bert')
+
+from run_eval_squad import SquadQA
+
+squad_qa = SquadQA()
+squad_qa.squad_setup_for_inference()
+print("SQUAD has been initialized:")
+
 from flask import Flask, render_template, request
 from werkzeug import secure_filename
 app = Flask(__name__)
 
 # TODO should these be globals
-view_data = {'subject' : 'nothing yet', 'question' : 'no question', 'answer' : 'no answer'}
+view_data = {'context' : 'nothing yet', 'question' : 'no question', 'answer' : 'no answer'}
 
-@app.route('/upload')
+@app.route('/squad_demo')
 def upload_file():
-   return render_template('my-form.html', result=view_data)
+   return render_template('squad-form.html', result=view_data)
 
 @app.route('/uploader', methods = ['GET', 'POST'])
 def uploader_file():
    if request.method == 'POST':
-      print(request.form['subject'])
-      view_data['subject'] = request.form['subject']
-      return render_template('my-form.html', result=view_data)
+      print("Context:\n")
+      print(request.form['context'])
+      print("Question:\n")
+      print(request.form['question'])
+      view_data['context'] = request.form['context']
+      return render_template('squad-form.html', result=view_data)
 
 
 if __name__ == '__main__':
